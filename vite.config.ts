e61b5dfd5a5d4a3f88524d3af75228aec96ee36f
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-08-09 12:29:37 +0300

    chore: added storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,55 @@
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  if (mode === "live-demo") {
+  if (mode === 'live-demo') {
     return {
-      base: "./",
+      base: './',
       plugins: [vue()],
       test: {
         globals: true,
       },
       resolve: {
         alias: {
-          "~": resolve(__dirname, "src", "siriUI"),
+          '~': resolve(__dirname, 'src', 'siriUI'),
         },
       },
       build: {
         rollupOptions: {
-          input: ["index.html"],
+          input: ['index.html'],
         },
       },
-    };
-  } else {
+    }
+  }
+  else {
     return {
       plugins: [vue()],
       test: {
         globals: true,
-        setupFiles: "src/setupTests.ts",
-        includeSource: ["src/**/*.spec.ts", "src/**/*.test.ts"],
+        setupFiles: 'src/setupTests.ts',
+        includeSource: ['src/**/*.spec.ts', 'src/**/*.test.ts'],
       },
       resolve: {
         alias: {
-          "~": resolve(__dirname, "src", "siriUI"),
+          '~': resolve(__dirname, 'src', 'siriUI'),
         },
       },
       build: {
         lib: {
-          entry: resolve(__dirname, "src/siriUI/index.ts"),
-          name: "Vue3ViteNpmTemplate",
-          fileName: "vue3-vite-npm-template",
+          entry: resolve(__dirname, 'src/siriUI/index.ts'),
+          name: 'Vue3ViteNpmTemplate',
+          fileName: 'vue3-vite-npm-template',
         },
         rollupOptions: {
-          external: ["vue"],
+          external: ['vue'],
           output: {
             globals: {
-              vue: "Vue",
+              vue: 'Vue',
             },
           },
         },
       },
-    };
+    }
   }
-});
+})
